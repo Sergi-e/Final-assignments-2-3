6621f601aee9a59aac04bb0228d44615cd1170db
sergi-e <ishserge16@gmail.com>
2025-11-19 14:46:11 +0000

    mutators added

--- Speaker.java
+++ Speaker.java
@@ -2,5 +2,47 @@ public class Speaker{
     private String speakerName;
     private String topic;
     private String biography;
-    prvate timeSlot;
+    private String timeslot;
+public Speaker(){}
+public Speaker(String speakerName, String topic, String biography, String timeslot){
+    this.speakerName = speakerName;
+    this.topic = topic;
+    this.biography = biography;
+    this.timeslot = timeslot;
+}
+     //Setters 
+    public void setSpeakerName(String speakerName) {
+        this.speakerName = speakerName;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public void setTimeslot(String timeslot) {
+        this.timeslot = timeslot;
+    }
+
+    //getters
+
+    public String getSpeakerName() {
+        return speakerName;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public String getTimeslot() {
+        return timeslot;
+    }
+    
 }
